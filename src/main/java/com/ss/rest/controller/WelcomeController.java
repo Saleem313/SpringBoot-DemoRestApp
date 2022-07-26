510cf359d4db8480c9ef039662230170ea62c137
Saleem313 <78643469+Saleem313@users.noreply.github.com>
2022-07-26 20:25:25 +0530

    Update WelcomeController.java

--- src/main/java/com/ss/rest/controller/WelcomeController.java
+++ src/main/java/com/ss/rest/controller/WelcomeController.java
@@ -23,15 +23,4 @@ public class WelcomeController {
     	System.out.println("*******Welcome to greeMsg method****************");
     	return"*****Asalamualikum******";
     }
-    
-    @GetMapping("/gree1")
-    public String greetMsg1() {
-    	return "happly coding";
-    }
-    
-    @GetMapping("/greet")
-    public  String welcome1( ) {
-    	System.out.println("********WelcomeController.welcome1***********");
-		return " *** Welcome to RestApi development ******** ";	
-    }
 }
