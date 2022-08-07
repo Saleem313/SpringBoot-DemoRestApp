2f36fa4e75541a48d99ccaef45c15c760f1897e7
SlimPrince777 <110737497+SlimPrince777@users.noreply.github.com>
2022-08-07 12:31:37 +0530

    Update WelcomeController.java

--- src/main/java/com/ss/rest/controller/WelcomeController.java
+++ src/main/java/com/ss/rest/controller/WelcomeController.java
@@ -16,38 +16,11 @@ public class WelcomeController {
 		return "Asslamualikum *** Welcome to RestApi development   ";
     	
     }
-    
-
-    @GetMapping("/salam")
-    public String greetMsg() {
-    	System.out.println("*******Welcome to greeMsg method****************");
-    	return"*****Asalamualikum******";
-    }
-
-    
-    @GetMapping("/greet")
-    public String greetMsg1() {
-    	return "happly coding";
-    }
-    
 	
     @GetMapping("/creator")
     public String greateCreator(){
        return "*****ALLAH**********";
     }
 	
-	@GetMapping("/newMethod")
-	public string newMethod(){
-		return "***new Method;
-	}
 
-	@GetMapping("/thanks")
-	public string greetMsg2(){
-		return "****thanks******";
-	}
-     @GetMapping("/newMethod")
-    public String greetMethod(){
-	return "***this is newMethod ****";
-    }
-    
 }
