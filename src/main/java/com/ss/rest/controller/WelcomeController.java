9197ae20c8eb9ce9f939425611904ce93640bf14
Saleem <saleem007865@gmail.com>
2022-08-07 12:24:18 +0530

    making two methods in controller

--- src/main/java/com/ss/rest/controller/WelcomeController.java
+++ src/main/java/com/ss/rest/controller/WelcomeController.java
@@ -9,36 +9,11 @@ public class WelcomeController {
     public WelcomeController() {
     	System.out.println("******************WelcomeController.WelcomeController()**************");
     }
-    
-    @GetMapping("/welcome")
-    public  String welcome() {
-    	System.out.println("********WelcomeController.welcome(***********)");
-		return "Asslamualikum *** Welcome to RestApi development   ";
-    	
-    }
-    
-
-    @GetMapping("/salam")
-    public String greetMsg() {
-    	System.out.println("*******Welcome to greeMsg method****************");
-    	return"*****Asalamualikum******";
-    }
-
-    
-    @GetMapping("/greet")
-    public String greetMsg1() {
-    	return "happly coding";
-    }
-    
 	
     @GetMapping("/creator")
     public String greateCreator(){
        return "*****ALLAH**********"
     }
 	
-	@GetMapping("/newMethod")
-	public String controllerMethod(){
-		return "***controller Method ***"
-	}
-    
+	
 }
